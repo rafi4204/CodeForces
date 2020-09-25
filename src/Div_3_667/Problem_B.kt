bde3d293800d1dd8117965f1c3e487205661352e
rafi <muhammad.farook@bs-23.net>
2020-09-25 16:10:12 +0600

    div3_ 667 problem b solved

--- src/Div_3_667/Problem_B.kt
+++ src/Div_3_667/Problem_B.kt
@@ -3,6 +3,7 @@ package Div_3_667
 
 import java.util.*
 import kotlin.math.abs
+import kotlin.math.min
 
 
 fun main() {
@@ -17,14 +18,29 @@ fun main() {
         val x = l[2].toLong()
         val y = l[3].toLong()
         val n = l[4].toLong()
-
-        if (n > abs(a - x)) {
-
+         var mn : Long =1000000000000000000
+        if ((a - n)<x && (b-(x-(a-n)))<y) {
+            mn= min(x*y,mn)
         }
-   //     val aa=List<Pair<Int,Int>>()
+        if ((a - n)<x && (b-(x-(a-n)))>=y) {
+            mn= min(x*(b-(x-(a-n))),mn)
+        }
+        if ((a - n)>=x ) {
+            mn= min((a - n)*b,mn)
+        }
+
+      if ((b - n)<y && (a-(y-(b-n)))<x) {
+            mn= min(x*y,mn)
+        }
+        if ((b - n)<y && (a-(y-(b-n)))>=x) {
+            mn= min(y*(a-(y-(b-n))),mn)
+        }
+        if ((b - n)>=y ) {
+            mn= min((b - n)*a,mn)
+        }
 
 
 
-        //println(res)
+        println(mn)
     }
 }
